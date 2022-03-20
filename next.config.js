e958e68ce3ed7e788abc682f33ee59046a6b51ef
saleehk <saleeh93@gmail.com>
2022-03-20 09:16:28 +0530

    added keystone and tailwind

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// module.exports = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+const { withKeystone } = require("@keystone-6/core/next");
+module.exports = withKeystone({
+  reactStrictMode: true,
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+
+import { InferGetStaticPropsType } from 'next';
+import Link from 'next/link';
+
+// Import the generated Lists API and types from Keystone
+import { query } from '.keystone/api';
+import { Lists } from '.keystone/types';
+
+type Post = {
+  id: string;
+  title: string;
+  slug: string;
+};
+
+// Home receives a `posts` prop from `getStaticProps` below
+export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-slate-500">
+      Hello world!
+    </h1>
+      <main style={{ margin: '3rem' }}>
+        <h1>Hello World! 👋🏻 </h1>
+        <ul>
+          {/* Render each post with a link to the content page */}
+          {posts.map(post => (
+            <li key={post.id}>
+              <Link href={`/post/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+// Here we use the Lists API to load all the posts we want to display
+// The return of this function is provided to the `Home` component
+export async function getStaticProps() {
+  const posts = await query.Post.findMany({ query: 'id title slug' }) as Post[];
+  return {
+    props: {
+      posts
+    }
+  };
+}
